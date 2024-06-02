323c93c8d7d098fe4186a8a7a6081cb7481753b2
xotourllife <xotourllife@naver.com>
2024-06-02 22:06:25 +0900

    계산 로직, treeView를 사용한 save 기능

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import sys
 import os
 from google.cloud import vision
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QPushButton, QFileDialog, QWidget, QTextEdit
-from PyQt5.QtGui import QPixmap, QImage
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QPushButton, QFileDialog, QWidget, QTextEdit, QFileSystemModel, QTreeView
+from PyQt5.QtGui import QPixmap, QImage, QFont, QIcon, QMovie, QPainter
+from PyQt5.QtCore import Qt, QTimer, QSize, QModelIndex
 import sympy as sp
 import cv2
 from PIL import Image
+import shutil
 
 
 # 환경 변수 설정 (다운로드한 JSON 키 파일의 경로를 입력해)
@@ -16,22 +17,68 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'C:\Users\XOTOURLLIFE\Desktop\js
 # Google Cloud Vision API 클라이언트 설정
 client = vision.ImageAnnotatorClient()
 
+WatchedOpening = False
 
+# class SplashScreen(QSplashScreen):
+#     def __init__(self, gif_path):
+#         # Create a QLabel with the QMovie as its content
+#         splash_pix = QPixmap()
+#         super().__init__(splash_pix)
+#         self.movie = QMovie(gif_path)
+#         self.setMovie(self.movie)
+#         self.movie.start()
+
+#     def setMovie(self, movie):
+#         self.movie = movie
+#         self.movie.setParent(self)
+#         self.movie.frameChanged.connect(self.repaint)
+
+#     def paintEvent(self, event):
+#         painter = QPainter(self)
+#         pixmap = self.movie.currentPixmap()
+#         painter.drawPixmap(0, 0, pixmap)
+    
 class MathSolverApp(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
-        
+   
     def initUI(self):
-        self.setWindowTitle('Handwritten Math Solver')
+        self.setWindowTitle('Mathemagics')
         self.setGeometry(100, 100, 800, 600)
-        
-        self.label = QLabel(self)
+        self.setWindowIcon(QIcon('./mathemagics/source/mathemagics_icon.jpg'))
+ 
+        self.icon_label = QLabel(self)
+        self.icon_label.setStyleSheet('background-color: gray;')
+
+        app_icon = QPixmap('./mathemagics/source/mathemagics_width_icon.png')
+        self.icon_label.setAlignment(Qt.AlignCenter)
+        app_icon = app_icon.scaledToHeight(100, Qt.SmoothTransformation)
+        self.icon_label.setPixmap(app_icon)
+
+        self.label = QLabel('미리 보기', self)
         self.label.setGeometry(150, 50, 500, 300)
+
+        self.imageLabel = QLabel('이미지가 불러와지지 않았습니다.', self)
+        self.imageLabel.setGeometry(150, 50, 500, 300)
+        self.imageLabel.setFont(QFont('Helvetica', 12))
+        self.imageLabel.setStyleSheet('color: black; background-color: yellow; padding: 30px; border: 4px solid black; border-radius: 4px;')
+
+        self.imageLabel.setAlignment(Qt.AlignCenter)
+
+        self.label2 = QLabel('라이브러리', self)
+        self.model =  QFileSystemModel()
+        self.model.setRootPath('./mathemagics/source/saved')
+
+        self.tree = QTreeView()
+        self.tree.setModel(self.model)
+        self.tree.setRootIndex(self.model.index('./mathemagics/source/saved'))
+        self.tree.clicked.connect(self.onFileClicked)
+
         
         self.resultLabel = QTextEdit('Result: ', self)
         self.resultLabel.setReadOnly(True)
-        self.resultLabel.setGeometry(150, 400, 500, 50)
+        self.resultLabel.setGeometry(150, 400, 100, 50)
         
         self.button = QPushButton('Load Image', self)
         self.button.setGeometry(150, 500, 100, 50)
@@ -39,17 +86,50 @@ class MathSolverApp(QMainWindow):
         
         self.widget = QWidget(self)
         self.layout = QVBoxLayout()
+        self.layout.addWidget(self.icon_label)
         self.layout.addWidget(self.label)
+        self.layout.addWidget(self.imageLabel)
+        self.layout.addWidget(self.label2)
+        self.layout.addWidget(self.tree)
         self.layout.addWidget(self.resultLabel)
         self.layout.addWidget(self.button)
         self.widget.setLayout(self.layout)
         self.setCentralWidget(self.widget)
 
+    
+    # def RunAnimation(self, Gif_path, duration):
+    #     self.movie = QMovie(Gif_path)
+    #     self.movie.setScaledSize(QSize(800, 600))
+    #     self.Frame.setMovie(self.movie)
+    #     self.movie.start()
+
+    #     timer = QTimer(self)
+    #     timer.singleShot(duration, self.Frame.clear)
+
     def loadImage(self):
         options = QFileDialog.Options()
         fileName, _ = QFileDialog.getOpenFileName(self, "Open Image File", "", "Image Files (*.png *.jpg *.bmp)", options=options)
         if fileName:
+            self.showImage(fileName)
             self.processImage(fileName)
+            destination = './mathemagics/source/saved/' + os.path.basename(fileName)
+            shutil.copy2(fileName, destination)
+    
+    def onFileClicked(self, index: QModelIndex):
+        file_path = self.model.filePath(index)
+        # self.showImage(file_path)
+        # self.processImage(file_path)
+
+        if file_path.lower().endswith(('.jpg')):
+            self.showImage(file_path)
+            self.processImage(file_path)
+    
+
+    def showImage(self, fileName):
+        pixmap = QPixmap(fileName)
+        pixmap = pixmap.scaled(500, 300, Qt.KeepAspectRatio)
+        self.imageLabel.setPixmap(pixmap)
+        self.imageLabel.setAlignment(Qt.AlignCenter)
 
     def processImage(self, fileName):
         # OpenCV로 이미지 읽기
@@ -66,19 +146,44 @@ class MathSolverApp(QMainWindow):
                 raise Exception(f'{response.error.message}')
 
         result = (texts[0].description).replace('\n', '') if texts else ""
-        latex = self.convert_to_latex(result)
-        self.resultLabel.setText(f"Result: {latex}")
+        answer = self.math_function(result)
+        self.resultLabel.setText(f"Result: {answer}")
     
-    def convert_to_latex(self, text):
+    def math_function(self, text):
         """Converts recognized text to LaTeX code."""
-        # 간단한 예제로서 변환 과정이 복잡할 수 있음
-        # 여기서는 단순히 인식된 텍스트를 반환
-        # 실제 구현에서는 수식을 인식하고 LaTeX으로 변환하는 로직이 필요
-        return text
+        refined_text = self.refine_text(text)
+        try:
+            answer = str(eval(refined_text))
+        except Exception as e:
+            answer = "제대로 된 수식을 입력해주세요."
+        output = text + "\n" + answer
+        return output
+    
+    def refine_text(self, text):
+        """Refines the recognized text."""
+        refined_text = text.replace(" ", "")
+        refined_text = refined_text.replace("=", "")
+        refined_text = refined_text.replace("x", "*")
+        refined_text = refined_text.replace("X", "*")
+        refined_text = refined_text.replace("−", "-")
+        refined_text = refined_text.replace("÷", "/")
+        return refined_text
+
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+
+    # opening = SplashScreen('./mathemagics/source/animation.gif')
+    # opening.show()
+
+    # def start_app():
+    #     opening.close()
+    #     main_window = MathSolverApp()
+    #     main_window.show()
+
+    # QTimer.singleShot(2000, start_app)
     ex = MathSolverApp()
     ex.show()
     sys.exit(app.exec_())
+
